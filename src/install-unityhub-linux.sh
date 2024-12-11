01361570632c19006286501cd0fa98f04bec16f9
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-12-11 12:15:21 -0500

    unity-setup@1.0.13
    - install ffmpeg on linux machines to fix missing audio

--- src/install-unityhub-linux.sh
+++ src/install-unityhub-linux.sh
@@ -5,7 +5,7 @@ echo "::group::Installing Unity Hub..."
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends unityhub
+sudo apt-get install -y --no-install-recommends unityhub ffmpeg
 sudo apt-get clean
 sudo sed -i 's/^\(.*DISPLAY=:.*XAUTHORITY=.*\)\( "\$@" \)2>&1$/\1\2/' /usr/bin/xvfb-run
 sudo printf '#!/bin/bash\nxvfb-run --auto-servernum /opt/unityhub/unityhub "$@" 2>/dev/null' | sudo tee /usr/bin/unity-hub >/dev/null
