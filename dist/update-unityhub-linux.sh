68ccd3ebeb5ceaa89ae7e761cc7d96a80d91f965
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-05-05 12:46:19 -0400

    --allow-releaseinfo-change

--- dist/update-unityhub-linux.sh
+++ dist/update-unityhub-linux.sh
@@ -3,4 +3,4 @@ set -e
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends --only-upgrade unityhub
+sudo apt-get install -y --no-install-recommends --only-upgrade --allow-releaseinfo-change unityhub
